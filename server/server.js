a4d4915aa580d8e2120e4ba1f249ea735297e052
hyunsojung-dev <thwjd2525@gmail.com>
2020-02-13 17:48:36 +0900

    network error test4

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(function(req, res, next) {
 // 소스코드 수정 후 npm run-script build > node ./index.js 시작
 console.log(path.resolve(__dirname,'../build'));
 
-app.use('http://hyunsojung-dev.github.io/react_hsj/', express.static(path.resolve(__dirname,'../build')));
+app.use('https://hyunsojung-dev.github.io/react_hsj/', express.static(path.resolve(__dirname,'../build')));
 
 //Router
 //라우터 설정 에러 문제 problem url 이후의 router 설정이기 때문에 /Problem/ 으로 나타내야함.
