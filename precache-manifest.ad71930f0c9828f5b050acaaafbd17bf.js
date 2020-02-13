5d763f4c86b9c29fce4e15598c5b0cb31adfa23a
hyunsojung-dev <thwjd2525@gmail.com>
2020-02-13 13:36:03 +0900

    Updates

---
+++ precache-manifest.ad71930f0c9828f5b050acaaafbd17bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d48051668bc2b1fdb5c1ef1b5829ca",
+    "url": "/react_hsj/index.html"
+  },
+  {
+    "revision": "ad65b73b25df646447a3",
+    "url": "/react_hsj/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cf28683458d59d3c0386",
+    "url": "/react_hsj/static/js/2.2ba3ca8d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_hsj/static/js/2.2ba3ca8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad65b73b25df646447a3",
+    "url": "/react_hsj/static/js/main.ff659768.chunk.js"
+  },
+  {
+    "revision": "01152505fe9c2175023e",
+    "url": "/react_hsj/static/js/runtime-main.bdb9c6da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_hsj/static/media/logo.5d5d9eef.svg"
+  }
+]);
